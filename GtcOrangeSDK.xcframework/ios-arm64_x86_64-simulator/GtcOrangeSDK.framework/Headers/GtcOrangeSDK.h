621807eb72bc7d0d846959ed1005021c3f4025e8
Alper Kayabasi <kayabasialper@gmail.com>
2021-11-21 16:45:47 +0300

    xcframework push.

---
+++ GtcOrangeSDK.xcframework/ios-arm64_x86_64-simulator/GtcOrangeSDK.framework/Headers/GtcOrangeSDK.h
@@ -0,0 +1,18 @@
+//
+//  GtcOrangeSDK.h
+//  GtcOrangeSDK
+//
+//  Created by Alper Kayabasi on 21.11.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GtcOrangeSDK.
+FOUNDATION_EXPORT double GtcOrangeSDKVersionNumber;
+
+//! Project version string for GtcOrangeSDK.
+FOUNDATION_EXPORT const unsigned char GtcOrangeSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GtcOrangeSDK/PublicHeader.h>
+
+
